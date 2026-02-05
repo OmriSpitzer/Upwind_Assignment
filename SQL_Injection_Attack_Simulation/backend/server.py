b993b58824e1adc4c924d6b1898a93391ab4ace1
OmriSpitzer <130233699+OmriSpitzer@users.noreply.github.com>
2026-02-05 08:57:36 +0200

    Malware Analysis
    GUI change, works with VM Ubuntu, minor fixes

--- SQL_Injection_Attack_Simulation/backend/server.py
+++ SQL_Injection_Attack_Simulation/backend/server.py
@@ -8,80 +8,152 @@ from hashlib import sha224
 app = Flask(__name__)
 CORS(app)
 
+# Load environment variables
 load_dotenv()
-PORT = os.getenv("PORT")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
 
-conn = None
-cursor = None
+# Database connection variables
+conn = None             # Database connection object
+cursor = None           # Database cursor object
 
-try:
-    conn = mysql.connector.connect(
-        host=MYSQL_HOST, 
-        user=MYSQL_USER, 
-        password=MYSQL_PASSWORD, 
-        database=MYSQL_DATABASE, 
-        port=MYSQL_PORT
+"""
+Connection function to the MySQL database
+@return: True if connected successfully, False otherwise
+"""
+def connect():
+    global conn, cursor
+    try:
+        # Connect to the MySQL database
+        conn = mysql.connector.connect(
+            host=MYSQL_HOST, 
+            user=MYSQL_USER, 
+            password=MYSQL_PASSWORD, 
+            database=MYSQL_DATABASE, 
+            port=MYSQL_PORT
         )
-    cursor = conn.cursor(dictionary=True)
-    print("\n- - - Connected to MySQL - - -\n")
-except Exception as e:
-    print("Error connecting to MySQL: " + str(e))
 
-def minimize_details(data):
-    for item in data:
-        item.pop('password')
-        item.pop('hash_pass')
-    return data
+        # Create a cursor object to interact with the database
+        cursor = conn.cursor(dictionary=True)
 
-def non_secure_login(username, password):
-    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    result = minimize_details(result)
-    return {'success': True, 'data': result}
+        print("\n- - - Connected to MySQL - - -\n")
+        return True
+    except Exception as e:
+        print("Error connecting to MySQL: " + str(e))
+        return False
 
+"""
+Disconnection function from the MySQL database
+@return: True if disconnected successfully, False otherwise
+"""
+def disconnect():
+    global conn, cursor
+    try:
+        # Close the cursor object
+        cursor.close()
+
+        # Close the database connection
+        conn.close()
+
+        print("\n- - - Disconnected from MySQL - - -\n")
+        return True
+    except Exception as e:
+        print("Error disconnecting from MySQL: " + str(e))
+        return False
+
+"""
+Secure login function
+@param username: username to login with
+@param password: password to login with
+@return: dictionary with the success flag and user data
+"""
 def secure_login(username, password):
+    # Empty validation check for username and password
     if not username or not password:
         return {'success': False, 'data': "No username or password inserted"}
 
     try:
+        # Hash the password given
         hashed_password = sha224(password.encode()).hexdigest()
-        print(f'Hashed password: {hashed_password}')
-        query = f"SELECT * FROM users WHERE username = %s AND hash_pass = %s"
+
+        # Query with parameter placeholders and specific fields to retrieve
+        query = f"SELECT country, email, id, name, username FROM users WHERE username = %s AND hash_pass = %s"
+        
+        # Execute the query with the parameters
         cursor.execute(query, (username, hashed_password))
+
+        # Fetch the result
         result = cursor.fetchall()
-        result = minimize_details(result)
-        return {'success': True, 'data': result}
+        if not result:
+            return {'success': False, 'data': "User not found"}
+        else:
+            return {'success': True, 'data': result}
     except Exception as e:
+        # If an error occurs, return the error message
         print("Error logging in: " + str(e))
         return {'success': False, 'data': "Error logging in"}
+
+"""
+Non-secure login function
+@param username: username to login with
+@param password: password to login with
+@return: dictionary with the success flag and user data
+"""
+def non_secure_login(username, password):
+    # Simple query
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    
+    # Execute the query
+    cursor.execute(query)
+    
+    # Fetch the result
+    result = cursor.fetchall()
+
+    if not result:
+        return {'success': False, 'data': "User not found"}
+    else:
+        return {'success': True, 'data': result}
 
 @app.route("/login", methods=["POST"])
 def login():
     try:
+        # Connect to the MySQL database
+        is_connected = connect()
+        if not is_connected:
+            return jsonify(response="Error connecting to database", status=500)
+
+        # Get variables from the request
         username = request.json.get("username")
         password = request.json.get("password")
         isSecure = request.json.get("isSecure")
 
-        print(f"\nLogin request received: {username} {password} {isSecure}")
+        print(f"\nLogin request received:")
+        print(f"Username: {username}, Password: {password.replace(password, '*' * len(password))}, Is Secure: {isSecure}")
 
+        # Perform the login depending on the secure mode
         result = non_secure_login(username, password) if not isSecure else secure_login(username, password)
 
+        # Disconnect from the MySQL database
+        is_disconnected = disconnect()
+        if not is_disconnected:
+            return jsonify(response="Error disconnecting from database", status=500)
+
+        # If the login is successful, return the user data
         if result['success']:
             data = result['data']
             print(f'Got users: {len(data)}\n')
             return jsonify(response="Login successful", status=200, userData=data)
         else:
-            print(f'Error logging in: {data}\n')
-            return jsonify(response="Error logging in", status=500)
+            # If the login is not successful, return the error message
+            print(f'Error logging in: {result["data"]}\n')
+            return jsonify(response=result["data"], status=500)
     except Exception as e:
+        # If an error occurs, return the error message
         print("Error logging in: " + str(e))
         return jsonify(response="Error logging in", status=500)
 
 if __name__ == "__main__":
-    app.run(port=PORT, debug=True)
+    app.run(port=8001, debug=True)
--- SQL_Injection_Attack_Simulation/myapp/src/App.jsx
+++ SQL_Injection_Attack_Simulation/myapp/src/App.jsx
@@ -1,74 +1,127 @@
 import { useState } from 'react'
 import './App.css'
 import axios from "axios"
+
+// Server URL
 const SERVER_URL = "http://localhost:8001"
 
+/**
+ * App component
+ * @returns {JSX.Element}
+ */
 function App() {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [userData, setUserData] = useState(null)
-  const [alert, setAlert] = useState(null)
-  const [isSecure, setIsSecure] = useState(false)
+  // State variables
+  const [username, setUsername] = useState("")        // Username input
+  const [password, setPassword] = useState("")        // Password input
+  const [userData, setUserData] = useState(null)      // Users retrieved from the database
+  const [alert, setAlert] = useState(null)            // Alert message
+  const [isSecure, setIsSecure] = useState(false)     // Flag if using secure mode
 
+  // Functions
+  /**
+   * Initialize state variables
+   */
   const initializeState = () => {
     setAlert(null)
     setUserData(null)
   }
 
-  const isValid = () => {
-    if (!username) {
-      setAlert("Please fill in the username field")
-      return false
-    }
-    if (!password) {
-      setAlert("Please fill in the password field")
-      return false
-    }
-    return true
+  /**
+   * Initialize input fields
+   */
+  const initializeInputs = () => {
+    setUsername("")
+    setPassword("")
   }
 
+  /**
+   * Empty validation check for username and password
+   * @returns {boolean}
+   */
+  const isValid = () => {
+    let isValid = true
+
+    // Username check
+    if (!username) {
+      setAlert("Please fill in the username field")
+      isValid = false
+    }
+    // Password check
+    else if (!password) {
+      setAlert("Please fill in the password field")
+      isValid = false
+    }
+
+    return isValid
+  }
+
+  /**
+   * Login function
+   * @returns {void}
+   */
   const login = async () => {
+    // Validation check
     if (!isValid()) {
       setAlert("Please fill in the username and password fields")
       return
     }
+
+    // Initialize state variables
     initializeState()
 
     try {
+      // Send the login request to the server
       const response = await axios.post(`${SERVER_URL}/login`, { username, password, isSecure })
 
       if (response.status === 200) {
-        setUserData(response.data.userData)
+        // Successfully received the response from the server
+        const userData = response.data.userData
+        setUserData(userData)
+        if (!userData?.length) {
+          setAlert("User not found")
+        }
       } else {
-        setAlert("Error logging in")
+        // Error received from the server
+        const error = response.data.response
+        setAlert(error)
       }
     } catch (error) {
+      // Error sending to the server
       setAlert("Error logging in: " + error.message)
     }
   }
+
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center">
+    <div className="h-full w-full flex flex-col items-center justify-center">
       <div className="h-3/4 w-full flex flex-col items-center justify-start gap-10 rounded-md p-10 bg-gray-200 border-5 border-gray-400">
+        {/* Title */}
         <h1>SQL Injection Attack Simulation</h1>
 
         <div className="flex flex-col items-center justify-start gap-5">
+          {/* Username and password input */}
           <div className="flex flex-row items-center justify-start gap-5">
+            {/* Username input */}
             <h2>Username :</h2>
             <input type="text" placeholder="ex. user123" value={username} onChange={(e) => setUsername(e.target.value)} />
 
+            {/* Password input */}
             <h2>Password :</h2>
             <input type="text" placeholder="ex. password123" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-            <button onClick={() => { initializeState(); setUsername(""); setPassword(""); }}>Reset</button>
+            {/* Reset button */}
+            <button onClick={() => { initializeState(); initializeInputs(); }}>Reset</button>
           </div>
 
-
-
+          {/* Login options */}
           <div className="flex flex-row items-center justify-start gap-4">
+            {/* Regular login button */}
             <h2> Regular Login:</h2>
             <button onClick={login}>Login</button>
+
+            {/* SQL Injection button */}
             <button onClick={() => setPassword("' OR '1'='1")}>SQL Injection</button>
 
+            {/* Secure mode toggle button */}
             <h2>Secure:</h2>
             <button
               className={isSecure ? 'btn-secure' : 'btn-not-secure'}
@@ -78,17 +131,22 @@ function App() {
             </button>
           </div>
 
+          {/* Alert message */}
           <p className='p-2 text-red-500'>{alert || ''}</p>
 
+          {/* Result output box */}
           <div className='flex flex-col w-full items-center justify-start gap-3'>
             <h1>Result</h1>
 
+            {/* Result output */}
             <div className='w-full border-2 rounded-md p-2 flex flex-col items-start justify-start gap-2 overflow-y-auto'>
               {userData && userData.length > 0 ? (
-                <div className='flex flex-row w-full justify-between'>
+                <div className='grid grid-cols-2 w-full gap-3'>
+                  {/* User data */}
                   {userData.map((user, index) => (
-                    <div className='flex flex-col gap-1' key={index}>
+                    <div className='flex flex-col w-full gap-1 border-b-10 border-gray-300 rounded-md pb-2' key={index}>
                       {Object.entries(user).map(([key, value]) => (
+                        // User details
                         <div className='flex flex-row items-center justify-start gap-3' key={key}>
                           <span className='font-semibold'>{key}:</span>
                           <span>{String(value)}</span>
@@ -98,6 +156,7 @@ function App() {
                   ))}
                 </div>
               ) : (
+                // No user data received
                 <h2>No user data</h2>
               )}
             </div>
